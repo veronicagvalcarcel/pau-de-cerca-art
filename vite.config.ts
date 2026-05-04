3090f6aca924c36c03c5bed41f8da2eafcd7442a
veronicagvalcarcel <veronicagelabert8@gmail.com>
2026-05-04 12:40:38 +0200

    Fix: Comment out allowedHosts to enhance security in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         host: 'localhost',
         port: 5173,
       },
-      allowedHosts: ['localhost', '127.0.0.1'],
+      // allowedHosts: ['localhost', '127.0.0.1', 'trycloudflare.com'],
     },
   };
 });
